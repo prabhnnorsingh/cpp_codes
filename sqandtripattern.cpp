3bc2479399a69eb7e73585f00f7cdf9786f46da2
prabhnoor singh <sprabhnoor720@gmail.com>
2023-08-30 22:40:48 +0530

    Square and triangle patterns

---
+++ sqandtripattern.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout << i;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout << j;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            cout << n-j+1;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            char ch = 'A' +j -1;
+            cout << ch;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=n)
+        {
+            char ch = ('A' + i-1) +j -1;
+            cout << ch;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+
+
+    /*int n;
+    cin >> n;
+
+    int i =1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j<=i)
+        {
+            char ch = 'A' + i-1;
+            cout << ch;
+            j += 1;
+        }
+        cout << endl;
+        i +=1;   
+    }*/
+
+}
