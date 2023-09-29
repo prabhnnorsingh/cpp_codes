4ead032f29a49a80365233316fc89ec132fa4b4c
prabhnoor singh <sprabhnoor720@gmail.com>
2023-09-29 18:35:12 +0530

    binary search

--- searching-sorting/linear_search.cpp
+++ searching-sorting/linear_search.cpp
@@ -1,6 +1,6 @@
 // Linear search
 
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 
 int linearsearch(int arr[],int n ,int key){
@@ -30,6 +30,53 @@ int main(){
     cin >> key;
 
     cout << "The index of the element is:-" << linearsearch(arr, n, key);
+}*/
+
+
+// Binary search
+
+#include <iostream>
+using namespace std;
+
+int binarysearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size-1;
+    int mid = start + ((end - start)/2);      // It is same as (start + end)/2
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if (arr[mid] << key)
+        {
+            start = mid + 1;
+        }
+
+        else{
+            end = mid - 1;
+        }
+        mid = start + ((end - start)/2);
+          
+    }
+    
 }
 
 
+int main() {
+    cout << "Enter the number of elements of array:-" << endl;
+    int size;
+    cin >> size;
+    cout << "Enter the elements of array:-" << endl << "Please enter the array in Ascending or Descending order" << endl;;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter the element you want to search:-" << endl;
+    int key;
+    cin >> key;
+
+    cout << "The index of the element is: " << binarysearch(arr, size, key);
+}
