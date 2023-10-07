ba45d9654468984a02cf1c07dddeb4b9eb1110a6
prabhnoor singh <sprabhnoor720@gmail.com>
2023-10-08 00:06:11 +0530

    painters partition problem

--- searching-sorting/linear_search.cpp
+++ searching-sorting/linear_search.cpp
@@ -86,3 +86,69 @@ int main() {
 
     cout << "The index of the element is: " << binarysearch(arr, size, key);
 }*/
+
+
+// Painters problem
+
+#include <iostream>
+using namespace std;
+
+bool ispossible(int arr[],int n,int m,int mid)
+{
+    int paintersum = 1;
+    int totalunits = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (totalunits + arr[i] <= mid)
+        {
+            totalunits += arr[i];
+        }
+        else
+        {
+            paintersum ++;
+            if (paintersum > m || arr[i]>mid)
+            {
+                return false;
+            }
+            totalunits = arr[i];
+            
+        }
+    }
+    return true;
+    
+}
+
+int mintimeallocated(int arr[],int n, int m)
+{
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    int e = sum;
+    int ans = -1;      
+    while (s<e)
+    {
+        int mid = s + ((e-s)/2);    // (s + e)/2;
+        if (ispossible(arr, n, m, mid))
+        {
+            ans = mid;
+            e = mid -1;
+        }
+        else{
+            s = mid +1;
+        }    
+    }
+    return ans;
+    
+}
+
+int main()
+{
+    int arr[]={5,5,5,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int m = 2;
+    cout << "Minimum time needed is : " << mintimeallocated(arr,n,m) << endl;
+}
+
