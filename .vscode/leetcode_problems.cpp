ccc3fa620b52b59f2ed487a813e381a63cfe026f
prabhnoor singh <sprabhnoor720@gmail.com>
2023-10-22 20:21:15 +0530

    check if array is sorted and rotated

--- .vscode/leetcode_problems.cpp
+++ .vscode/leetcode_problems.cpp
@@ -131,6 +131,37 @@ int main()
 
 // check if array is sorted and rotated
 
+/*int main()
+{
+    int arr[]={3,4,5,1,2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i-1] > arr[i])
+        {
+            count++;
+        }
+        
+    }
+    if (arr[n-1] > arr[0])
+    {
+        count ++;
+    }
+    if (count <= 1)
+    {
+        cout << "true";
+    }
+    else
+    {
+        cout << "false";
+    }
+       
+}*/
+
+
+// Sum of 2 arrays
+
 int main(){
-    
+     
 }
